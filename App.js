955d00db3bf20f60425441277e6f2b60ca410dd1
orearey <144205790+orearey@users.noreply.github.com>
2023-09-11 10:34:09 +0300

    React

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to status</Text>
+      <Text> OOOOPPPPEEEENNNN</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#435334',
+    alignItems: 'center',
+    color: '#fff',
+    justifyContent: 'center',
+  },
+});
